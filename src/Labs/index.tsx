f2800161baaf3c7afc353985e90b7aeaffb7e1c5
aryanaher2299 <aryanaher24@gmail.com>
2024-05-13 19:01:46 -0400

    added Name

--- src/Labs/index.tsx
+++ src/Labs/index.tsx
@@ -6,6 +6,8 @@ import Lab3 from "./Lab3";
 export default function Labs() {
   return (
     <div>
+      <h1>Aryan Aher</h1>
+      <h2>Section 01</h2>
       <h1>Labs</h1>
       <TOC />
       <Routes>
