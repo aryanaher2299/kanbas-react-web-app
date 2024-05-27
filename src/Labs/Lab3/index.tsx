213a8358df7f844c94cb7669415dd1d706db2b1b
aryanaher2299 <aryanaher24@gmail.com>
2024-05-27 10:24:13 -0400

    a3- first commit

--- src/Labs/Lab3/index.tsx
+++ src/Labs/Lab3/index.tsx
@@ -1,8 +1,66 @@
+import AddingAndRemovingToFromArrays from "./AddingAndRemovingToFromArrays";
+import ArrayIndexAndLength from "./ArrayIndexAndLength";
+import ArrowFunctions from "./ArrowFunctions";
+import BooleanVariables from "./BooleanVariables";
+import ConditionalOutputIfElse from "./ConditionalOutputIfElse";
+import ConditionalOutputInline from "./ConditionalOutputInline";
+import FilterFunction from "./FilterFunction";
+import FindFunction from "./FindFunction";
+import FindIndex from "./FindIndex";
+import ForLoops from "./ForLoops";
+import IfElse from "./IfElse";
+import ImpliedReturn from "./ImpliedReturn";
+import JSONStringify from "./JSONStringify";
+import LegacyFunctions from "./LegacyFunctions";
+import MapFunction from "./MapFunction";
+import Objects from "./Objects";
+import SimpleArrays from "./SimpleArrays";
+import TemplateLiterals from "./TemplateLiterals";
+import TernaryOperator from "./TernaryOperator";
+import TodoItem from "./TodoItem";
+import VariableTypes from "./VariableTypes";
+import VariablesAndConstants from "./VariablesAndConstants";
+import TodoList from "./TodoList";
+import Spreading from "./Spreading";
+import FunctionDestructing from "./FunctionDestructing";
+import DestructingImports from "./DestructingImports";
+import Classes from "./Classes";
+
 export default function Lab3() {
-    return (
-      <div>
-        <h2>Lab 3</h2>
-      </div>
-    );
-  }
-  
+  return (
+    <div>
+      <h2>Lab 3</h2>
+      <VariablesAndConstants />
+      <VariableTypes />
+      <BooleanVariables />
+      <IfElse />
+      <TernaryOperator />
+      <ConditionalOutputIfElse />
+      <ConditionalOutputInline />
+      <LegacyFunctions />
+      <ArrowFunctions/>
+      <ImpliedReturn/>
+      <TemplateLiterals/>
+      <SimpleArrays/>
+      <ArrayIndexAndLength/>
+      <AddingAndRemovingToFromArrays/>
+      <ForLoops/>
+      <MapFunction/>
+      <FindFunction/>
+      <FindIndex/>
+      <FilterFunction/>
+      <JSONStringify/>
+      <Objects/>
+      <TodoItem/>
+      <TodoList/>
+      <Spreading/>
+      <FunctionDestructing/>
+      <DestructingImports/>
+      <Classes/>
+    
+
+      
+
+    </div>
+  );
+}
