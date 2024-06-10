15e9f86d1762a5b6fe0e53d92086696db8b4551e
aryanaher2299 <aryanaher24@gmail.com>
2024-06-10 18:08:42 -0400

    added server link

--- src/Labs/TOC.tsx
+++ src/Labs/TOC.tsx
@@ -48,6 +48,11 @@ export default function TOC() {
           My GitHub
         </a>
       </li>
+      <li className="nav-item">
+        <a id="wd-k" href="https://kanbas-node-server-app-nz0y.onrender.com/" className="nav-link">
+          My Server
+        </a>
+      </li>
     </ul>
   );
 }
